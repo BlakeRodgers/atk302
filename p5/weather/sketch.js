3639de4844e340b057eb8c5f4517bfa645346251
BlakeRodgers <54369219+BlakeRodgers@users.noreply.github.com>
2020-05-01 14:15:20 -0500

    weather

--- p5/weather/sketch.js
+++ p5/weather/sketch.js
@@ -2,6 +2,9 @@ var weather;
 var temp = 0;
 var weatherID = 0; // returned in the JSON weather element
 var myState = 0;
+var x = 0;
+var ws = 0;
+var temperatire = 0;
 
 
 function setup() {
@@ -10,7 +13,7 @@ function setup() {
   // HERE Is the call to get the weather. PLEASE PUT YOUR OWN ID IN HERE!!!
   // MINE WILL STOP WORKING IF EVERYONE USES IT!!!
 
-  var myJSONString = 'https://api.openweathermap.org/data/2.5/weather?zip=61701,us&units=imperial&';
+  var myJSONString = 'https://api.openweathermap.org/data/2.5/weather?zip=78728,us&units=imperial&';
   var myIDString = 'appid=85b99c5bb846b0a85273d2bf020d2da9'; // Take out the x’s and ADD YOUR ID!!!
   loadJSON(myJSONString + myIDString, gotData); // look - that gotData function happens when JSON comes back.
 }
@@ -19,7 +22,9 @@ function setup() {
 
 function gotData(data) {
   weather = data;
-  console.log(data); // for debugging purposes, print out the JSON data when we get it.
+  console.log(data);
+  ws = weather.wind.speed;
+  temperature = weather.main.temp; // for debugging purposes, print out the JSON data when we get it.
 }
 
 
@@ -33,9 +38,24 @@ function draw() {
 
     case 1:
     background(200) ;
+    fill('black');
     text("we have weather", 20, 20) ;
 // parse the weather object and put some text or images using at least 3 different weather data!
 text("the temperature is" + weather.main.temp, 20, 40);
+text("the humidity is" + weather.main.humidity, 20, 60);
+text("the windspeed is" + weather.wind.speed, 20, 80);
+
+fill('white');
+noStroke();
+ellipse(x, 250, 200, 100)
+x = x + ws;
+if (x > width) x = 0;
+
+
+fill('red');
+var tmp = 0;
+tmp = map(temperature, -10, 90, 2, height-10);
+rect(width-40, height-10, 30, -tmp);
       break;
 
   }
